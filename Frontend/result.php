1eaddd6567efa0337ee99f5e5d01692cd81d9089
Fabian Bees <fabianbees@web.de>
2020-03-30 11:55:51 +0200

    fix for result page.

--- Frontend/result.php
+++ Frontend/result.php
@@ -1,6 +1,17 @@
 <?php
+  // No error messages...
+  error_reporting(0);
+  // ALL error messages...
+  // error_reporting(E_ALL);
 
-# TODO ...
+  if (isset($_GET['search'])) {
+    $search = $_GET['search'];
+  } else {
+    $search = "";
+    echo "Search phrase missing...";
+  }
+
+  include "../WebCrawler/worker.php";
 
 ?>
 
@@ -52,74 +63,60 @@
 
         <?php
 
-          # TODO ...
-          
-          // Config
-          $cfg_limit_found_words = 100;
-          $cfg_limit_found_links_per_words = 100;
+          $db = new DBHandler();
       
-          // Include some connection info...
-          $credentials_file = "../db_settings.php";    
-          include $credentials_file;
-          $mysqli = new mysqli('127.0.0.1', $db_user, $db_pass, 'dhbw_crawler');
-          
-          // Connect to the database...
-          if ($mysqli->connect_errno) {
-              printf("MySQL connect failed: %s\n", $mysqli->connect_error);
-              exit;
-          }
           
           // Only if there is a search phrase...
           if ($search != "") {
-              // ...perform an SQL query for searching the word in the database. It may be found only once...
-              $sql = "SELECT * FROM word WHERE word LIKE '%$search%' LIMIT $cfg_limit_found_words;"; // LIMITED NUMBER OF RESULTS
-              if (!$result = $mysqli->query($sql)) {
-                  report_problem("Query failed to execute: ". $sql, $mysqli);
-                  exit;
-              } else {
-                  // Open the ul list and loop the word entries...
-                  echo "<ul>\n";
-                  $record_counter = 0;
-                  while ($word_item = $result->fetch_assoc()) {
+
+            // Open the ul list and loop the word entries...
+            echo "<ul>\n";
+            $record_counter = 0;
+
+            foreach ($db->get_matching_words($search) as $word_item) {
+
                       //echo "<li><a href='" . $_SERVER['SCRIPT_NAME'] . "?aid=" . $user['id'] . "'>\n";
                       echo "<li> Result(s) for search phrase [" . $word_item['word'] . "]:\n"; //  (id: " . $word_item['id'] . ")\n";
                       
-                      // Finally we query the links being connected to the word entry...
-                      $sql="SELECT * FROM wordlinks WHERE id_word = '" . $word_item['id'] . "'  LIMIT $cfg_limit_found_links_per_words;"; // LIMITED NUMBER OF RESULTS
-                      if (!$result2 = $mysqli->query($sql)) {
-                          report_problem("Query failed to execute: ". $sql, $mysqli);
-                          exit;
-                      } else {
-                          // Open the next level ul list and loop the wordlink entries ...
-                          echo "<ul>\n";
-                          while ($wordlink_item = $result2->fetch_assoc()) {
-                              echo "<li>\n";
-                              // Trace: echo "Wordlink found. The id_link is " . $wordlink_item["id_link"] . "</br>\n";
-                              
-                              // Finally we need the real link...
-                              $sql = "SELECT * FROM tbl_link WHERE id = '".$wordlink_item["id_link"]."';";
-                              if (!$result3 = $mysqli->query($sql)) {
-                                  report_problem("Query failed to execute: ". $sql, $mysqli);
-                                  exit;
-                              } else {
-                                  // We have found the link. So lets display it...
-                                  $a_link = $result3->fetch_assoc()['link'];
-                                  $record_counter++;
-                                  echo "$record_counter - <a href='" . $a_link . "'>" . $a_link . "</a>\n";
-                              }
-                              echo "</li>\n";
-                          }
-                          echo "</ul>\n";
+
+                      // Finally we query the links being connected to the word entry...                      
+
+                      $word_links = $db->get_word_links($word_item['id']);
+
+
+                      // Open the next level ul list and loop the wordlink entries ...
+                      echo "<ul>\n";
+
+                      foreach($word_links as $wordlink_item) {
+                            echo "<li>\n";
+                          // Trace: echo "Wordlink found. The id_link is " . $wordlink_item["id_link"] . "</br>\n";
+                          
+                          // Finally we need the real link...
+
+
+                          //$sql = "SELECT * FROM tbl_link WHERE id = '".$wordlink_item["id_link"]."';";
+
+                          $link = $db->get_link(wordlink_item["id_link"]);
+
+
+                          // We have found the link. So lets display it...
+                          $a_link = $link['link'];
+                          $record_counter++;
+                          echo "$record_counter - <a href='" . $a_link . "'>" . $a_link . "</a>\n";
+                          
+                          echo "</li>\n";
                       }
+
+
+                      echo "</ul>\n";
+                      
                       // echo $user['firstname'] . ' ' . $user['lastname'];
                       echo "</li>\n";
-                  }
-                  echo "</ul>\n";
 
-                  // Free the resources...
-                  $result->free();
-                  $mysqli->close();
-              }
+            }
+            echo "</ul>\n";
+              
+
           } else {
               echo "Oops, nothing found. => Enter a search phrase!</br>\n";
           }
